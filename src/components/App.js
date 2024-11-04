f9101a971102e70c977b672bb1ae49c020bdb110
Akanle Jeremy <akanlejeremy@gmail.com>
2024-11-04 14:04:03 +0300

    passing

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,25 @@
+import React, { useEffect, useState } from "react"
+
 // create your App component here
+function App() {
+    const API_URL = "https://dog.ceo/api/breeds/image/random"
+    const [dog, setDog] = useState()
+    const [load, setLoad] = useState(true)
+    
+    useEffect(() => {
+        fetch(API_URL)
+        .then((r) => r.json())
+        .then((data) => {
+            setDog(data)
+            setLoad(false)
+        })
+    }, [])
+    return (
+        <>
+        {load ? <p>Loading...</p> : null}
+        {dog ? <img src={dog.message} alt="A Random Dog"></img> : null}
+        </>
+    )
+}
+
+export default App
